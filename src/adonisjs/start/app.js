cd2c8f5a3a777e1ece817e44beeb0f53fb2dcbed
faguim <pantoja.ti@gmail.com>
2020-08-05 20:58:45 -0300

    fixing crash by @adonis/validatior

--- src/adonisjs/start/app.js
+++ src/adonisjs/start/app.js
@@ -18,8 +18,8 @@ const providers = [
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/shield/providers/ShieldProvider',
   '@adonisjs/session/providers/SessionProvider',
-  '@adonisjs/auth/providers/AuthProvider',
-  '@adonisjs/validator/providers/ValidatorProvider'
+  '@adonisjs/auth/providers/AuthProvider'
+  // '@adonisjs/validator/providers/ValidatorProvider'
 ]
 
 /*
