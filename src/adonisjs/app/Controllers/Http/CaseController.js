1833d1b95d2f1707f513f315f897112a4903c374
Marcos Mota <ra211893@ic.unicamp.br>
2020-08-06 18:02:16 -0300

    feat (create case) : CaseController request

--- src/adonisjs/app/Controllers/Http/CaseController.js
+++ src/adonisjs/app/Controllers/Http/CaseController.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const Env = use("Env")
+const axios = use("axios")
+
 
 class CaseController {
-    async index ({ view }) {
+    async fetch ({ view }) {
 
         const harena_manager_url = Env.get("HARENA_MANAGER_URL", "http://127.0.0.1:1020");
         const cases_url = harena_manager_url + "/cases"        
@@ -13,6 +15,24 @@ class CaseController {
                       }, (error) => {
                           console.log(error)
                       })
+    }
+
+    async store ({request, session, reponse}) {
+        try {
+            request.all()
+
+            await axios(config)
+            .then(function (endpoint_response) {
+                return response.redirect('/author/author.html')
+                })
+            .catch(function (error) {
+                // console.log(error);
+            });
+
+        }
+        catch (e) {
+            console.log(e)
+        }
     }
 }
 
