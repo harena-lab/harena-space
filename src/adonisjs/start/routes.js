bf6edb55204a99eed24e30c58936978d34e374b2
Heitor Mattosinho <heitormattosinho@gmail.com>
2020-08-15 00:37:37 -0300

    refactor (Controller): Created 'getCase' function
    This code was misplaced in 'routes.js'. Created the function 'getCase' in 'CaseController.js' and migrated the code.

--- src/adonisjs/start/routes.js
+++ src/adonisjs/start/routes.js
@@ -21,8 +21,8 @@ const axios = use("axios")
 
 Route.get(  'populate_modal', 'CaseController.populate_modal')
 
-Route.get('/', ({ view }) => 
-    view.render('index') 
+Route.get('/', ({ view }) =>
+    view.render('index')
 ).as('index')
 
 Route.get('institution-registration', async ({ view }) => {
@@ -47,53 +47,7 @@ Route.group(() => {
 
 
 
-Route.get('author', async ({ view, request }) => {
-   // View.global('name_you_like', function () {
-
-   //    return "ase"
-   //  })
-   try{
-      // const params = request.all();
-
-      const endpoint_url = Env.get("HARENA_MANAGER_URL") + "/api/v1/case/" +
-         request.input("id");
-         // "d2ad02da-b7e1-4391-9f65-4f93eeb4ca7f"
-
-      var config = {
-        method: 'get',
-        url: endpoint_url,
-        headers: {
-          'Authorization': 'Bearer ' + request.cookie('token')
-        }
-      };
-
-
-      await axios(config)
-        .then(function (endpoint_response) {
-
-            console.log(endpoint_response.data)
-          //return view.render('author.author')
-          let responseContent = endpoint_response.data
-          let caseId = responseContent.id;
-          let caseTitle = responseContent.title
-          let caseDescription = responseContent.description
-          let caseLanguage = responseContent.language
-          let caseInstitution = responseContent.institution
-          let caseDomain = responseContent.domain
-          let caseSpecialty = responseContent.specialty
-          let caseKeywords = responseContent.keywords
-          let caseOriginalDate = responseContent.original_date
-          return view.render('author.author',
-             {caseId, caseTitle, caseDescription, caseLanguage, caseInstitution, caseDomain, caseSpecialty, caseKeywords, caseOriginalDate})
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    } catch(e){
-      console.log(e)
-    }
-      return view.render('author.author')
-}).as('author_edit')
+Route.get('author', 'CaseController.getCase').as('author_edit')
 
 Route.get('home', ({ view }) => {
    return view.render('author.home')
