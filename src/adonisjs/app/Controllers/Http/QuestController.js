2615dd58c01f4cf87489cf65ca63dcb33ca1501f
Heitor Mattosinho <heitormattosinho@gmail.com>
2020-08-27 12:46:50 -0300

    feat (player cases): working get cases from quest

--- src/adonisjs/app/Controllers/Http/QuestController.js
+++ src/adonisjs/app/Controllers/Http/QuestController.js
@@ -11,13 +11,13 @@ class QuestController {
 		try{
       		const params = request.all()
 
-	      	let endpoint_url = Env.get("HARENA_MANAGER_URL") + "/api/v1/quest/cases" 
+	      	let endpoint_url = Env.get("HARENA_MANAGER_URL") + "/api/v1/quest/cases"
 
 		    var config = {
 		      method: 'get',
 		      url: endpoint_url,
 		      data: {
-		          questId: params.questId
+		          questId: params.id
 		          // questId: 'f89c9a67-d6a9-453b-afeb-cfbb079c2a25'
 		      },
 		      headers: {
@@ -28,15 +28,33 @@ class QuestController {
 
       		await axios(config)
       			.then(function (endpoint_response) {
-        			console.log(endpoint_response.data)
-          			return response.redirect('/player/home')
+        			console.log(endpoint_response.data);
+						//	const jsonResponse = response.json();
+						// 	const busResponse = []
+					  //   for (const c in jsonResponse) {
+					  //     busResponse.push({
+					  //       id: jsonResponse[c].id,
+					  //       title: jsonResponse[c].title,
+					  //     // svg : jsonResponse[c].svg
+					  //     })
+						// 		console.log('======================================================BUSSSSSS: '+busResponse);
+						// 		//return console.log(busResponse);
+						//
+						// }
+
+						const test = endpoint_response.data[0].id
+						return view.render('player.player-new-cases', {test});
       			})
+
 		    	.catch(function (error) {
 		        	console.log(error);
 		      	});
+
 	    } catch(e){
 	      console.log(e)
 	    }
+			return view.render('player.player-new-cases');
+
   	}
 }
 
