f5cc9e6ba89d202f9df2104a0db7d17d203c87c9
Heitor Mattosinho <heitormattosinho@gmail.com>
2020-09-08 13:26:17 -0300

    feat (redirect unlogged): redirect login if unlogged
    User is redirected to login if trying to access pages that require login to load properly

--- src/adonisjs/public/infra/token-validator.js
+++ src/adonisjs/public/infra/token-validator.js
@@ -1,12 +1,22 @@
 class HarenaController {
   constructor () {
+    // if (window.location.pathname != '/') {
+    //   this.redirectUnlogged();
+    // }else
+    //   this.checkToken();
 
+    window.location.pathname != '/' ? this.redirectUnlogged():this.checkToken()
 
-
-        //this.test();
 
   }
-  async checkToken (){
+  async checkToken (checked){
+
+    if(checked){
+      let elem = document.getElementById('header-login').firstElementChild;
+      elem.href='/logout';
+      elem.innerHTML='Log out';
+      return;
+    }
 
     if(DCCCommonServer.token != 'undefined'){
 
@@ -17,16 +27,19 @@ class HarenaController {
           Authorization: 'Bearer ' + DCCCommonServer.token
         }
       }
-      console.log('=== check token request')
-      console.log(DCCCommonServer.managerAddressAPI + 'auth/check')
+      // console.log('=== check token request')
+      // console.log(DCCCommonServer.managerAddressAPI + 'auth/checkToken')
       axios(config)
       .then(function (endpointResponse) {
         // return response.redirect('/')
-        console.log('=== check token response')
-        console.log(endpointResponse.data);
-        let elem = document.getElementById('header-login');
-        endpointResponse.data='jwt token true' ? elem.parentNode.removeChild(elem):console.log('shit');;
+        // console.log('=== check token response')
+        // console.log(endpointResponse.data);
 
+        if (endpointResponse.data='token valid') {
+          let elem = document.getElementById('header-login').firstElementChild;
+          elem.href='/logout';
+          elem.innerHTML='Log out';
+        }
       })
       .catch(function (error) {
         console.log('=== check token error')
@@ -34,13 +47,46 @@ class HarenaController {
       })
     }else{
       console.log('token invalid');
-      let elem = document.getElementById('header-logout');
-      elem.parentNode.removeChild(elem);
+      let elem = document.getElementById('header-login').firstElementChild;
+      elem.href='/login'
+      elem.innerHTML='Log in'
     }
 
+  }
+  async redirectUnlogged(){
+
+    $( document ).ready(function() {
+
+      if(DCCCommonServer.token != 'undefined'){
+
+        const config = {
+          method: 'GET',
+          url: DCCCommonServer.managerAddressAPI + 'auth/check',
+          headers: {
+            Authorization: 'Bearer ' + DCCCommonServer.token
+          }
+        }
+        console.log('=== check token request')
+        console.log(DCCCommonServer.managerAddressAPI + 'auth/check')
+        axios(config)
+        .then(function (endpointResponse) {
+
+          // console.log('=== check token response')
+          // console.log(endpointResponse.data);
+          endpointResponse.data == 'token valid' ? HarenaController.instance.checkToken(true):   window.location.href = '/login'
+
+        })
+        .catch(function (error) {
+          console.log('=== check token error')
+          console.log(error)
+        })
+      }else{
+        window.location.href = '/login'
+      }
+    });
   }
 }
 (function () {
   HarenaController.instance = new HarenaController()
-  HarenaController.instance.checkToken();
+
 })()
