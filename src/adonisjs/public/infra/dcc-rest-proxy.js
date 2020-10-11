de8f9faa9f1183340c3b15014b6f39e73538bb3e
Andre Santanche <santanche@gmail.com>
2020-10-11 19:14:28 -0300

    fix (dcc): merge problems

--- src/adonisjs/public/infra/dcc-rest-proxy.js
+++ src/adonisjs/public/infra/dcc-rest-proxy.js
@@ -1,28 +1,5 @@
 (function () {
 
-<<<<<<< Updated upstream
-  DCC.contentComponent(
-    'harena-login',
-    'dcc-rest',
-    {
-      environment: {
-        'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api
-      },
-      oas: {
-        paths: {
-          '{url-manager}/auth/login': {
-            'post': {
-              operationId: 'login',
-              parameters: [
-                {name: 'url-manager',
-                 in: 'path'},
-                {name: 'email',
-                 in: 'query'},
-                {name: 'password',
-                 in: 'query'},
-              ]
-            }
-=======
 DCC.component(
   'harena-login',
   'dcc-rest',
@@ -43,34 +20,15 @@ DCC.component(
               {name: 'password',
                in: 'query'},
             ]
->>>>>>> Stashed changes
           }
         }
       }
     }
-  )
+  }
+)
 
-<<<<<<< Updated upstream
-  DCC.contentComponent(
-    'harena-logout',
-    'dcc-rest',
-    {
-      environment: {
-        'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api
-      },
-      oas: {
-        paths: {
-          '{url-manager}/auth/logout': {
-            'post': {
-              operationId: 'logout',
-              parameters: [
-                {name: 'url-manager',
-                 in: 'path'}
-              ]
-            }
-=======
 DCC.component(
-  'harena-roles',
+  'harena-logout',
   'dcc-rest',
   {
     environment: {
@@ -78,39 +36,20 @@ DCC.component(
     },
     oas: {
       paths: {
-        '{url-manager}/admin/roles': {
-          'get': {
-            operationId: 'roles',
+        '{url-manager}/auth/logout': {
+          'post': {
+            operationId: 'logout',
             parameters: [
               {name: 'url-manager',
                in: 'path'}
             ]
->>>>>>> Stashed changes
           }
         }
       }
     }
-  )
+  }
+)
 
-<<<<<<< Updated upstream
-  DCC.contentComponent(
-    'harena-roles',
-    'dcc-rest',
-    {
-      environment: {
-        'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api
-      },
-      oas: {
-        paths: {
-          '{url-manager}/admin/roles': {
-            'get': {
-              operationId: 'roles',
-              parameters: [
-                {name: 'url-manager',
-                 in: 'path'}
-              ]
-            }
-=======
 DCC.component(
   'harena-cases',
   'dcc-rest',
@@ -129,58 +68,59 @@ DCC.component(
               {name: 'questId',
                in: 'path'}
             ]
->>>>>>> Stashed changes
           }
         }
       }
     }
-  )
+  }
+)
 
-  DCC.contentComponent(
-    'harena-cases',
-    'dcc-rest',
-    {
-      environment: {
-        'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api
-      },
-      oas: {
-        paths: {
-          'http://localhost:10020/api/v1/author/quest/cases?questId={questId}': {
-            'get': {
-              operationId: 'cases',
-              parameters: [
-                {name: 'url-manager',
-                 in: 'path'},
-                {name: 'questId',
-                 in: 'path'}
-              ]
-            }
+DCC.component(
+  'harena-cases',
+  'dcc-rest',
+  {
+    environment: {
+      'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api
+    },
+    oas: {
+      paths: {
+        'http://localhost:10020/api/v1/author/quest/cases?questId={questId}': {
+          'get': {
+            operationId: 'cases',
+            parameters: [
+              {name: 'url-manager',
+               in: 'path'},
+              {name: 'questId',
+               in: 'path'}
+            ]
           }
         }
       }
     }
-  )
-  DCC.contentComponent(
-    'harena-categories',
-    'dcc-rest',
-    {
-      environment: {
-        'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api
-      },
-      oas: {
-        paths: {
-          '{url-manager}/author/category/list': {
-            'get': {
-              operationId: 'categories',
-              parameters: [
-                {name: 'url-manager',
-                 in: 'path'}
-              ]
-            }
+  }
+)
+
+DCC.component(
+  'harena-categories',
+  'dcc-rest',
+  {
+    environment: {
+      'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api
+    },
+    oas: {
+      paths: {
+        '{url-manager}/author/category/list': {
+          'get': {
+            operationId: 'categories',
+            parameters: [
+              {name: 'url-manager',
+               in: 'path'}
+            ]
           }
         }
       }
     }
-  )
+  }
+)
 
 })()
