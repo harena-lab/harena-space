c702f257a846592f8f11fe831eef665517ac238d
Heitor Mattosinho <heitormattosinho@gmail.com>
2020-10-12 19:52:44 -0300

    refactor (token-validator): redirect to main page in logout

--- src/adonisjs/public/infra/dcc-basic.js
+++ src/adonisjs/public/infra/dcc-basic.js
@@ -1,13 +1,23 @@
 (function () {
 
-DCC.component(
-  'submit-login',
-  'dcc-submit',
-  {
-    pos: function(response) {
-      window.location.href = '/'
+  DCC.component(
+    'submit-login',
+    'dcc-submit',
+    {
+      pos: function(response) {
+        window.location.href = '/'
+        // console.log(response)
+      }
     }
-  }
-)
-
+  )
+  DCC.component(
+    'submit-logout',
+    'dcc-submit',
+    {
+      pos: function(response) {
+        window.location.href = '/'
+        // console.log(response)
+      }
+    }
+  )
 })()
