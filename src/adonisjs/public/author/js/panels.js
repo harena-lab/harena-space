d18aa354c309758cb383cdc91fae34d91da443fb
Heitor Mattosinho <heitormattosinho@gmail.com>
2021-02-05 17:30:24 -0300

    refactor(author/panels): better fit for comment section

--- src/adonisjs/public/author/js/panels.js
+++ src/adonisjs/public/author/js/panels.js
@@ -54,20 +54,20 @@ class Panels {
   }
 
   setupHiddenNavigator () {
-    this._navigationBlock.classList.remove('w-25')
+    // this._navigationBlock.classList.remove('w-25')
     this._navigationBlock.style.display = 'none'
-    this._knotMain.classList.remove('w-' + this._knotPanelSize)
-    this._knotPanelSize += 25
-    this._knotMain.classList.add('w-' + this._knotPanelSize)
+    // this._knotMain.classList.remove('w-' + this._knotPanelSize)
+    // this._knotPanelSize += 25
+    // this._knotMain.classList.add('w-' + this._knotPanelSize)
     this._buttonExpandNav.style.display = 'initial'
     this._buttonRetractNav.style.display = 'none'
   }
 
   setupVisibleNavigator () {
-    this._knotMain.classList.remove('w-' + this._knotPanelSize)
-    this._knotPanelSize -= 25
-    this._knotMain.classList.add('w-' + this._knotPanelSize)
-    this._navigationBlock.classList.add('w-25')
+    // this._knotMain.classList.remove('w-' + this._knotPanelSize)
+    // this._knotPanelSize -= 25
+    // this._knotMain.classList.add('w-' + this._knotPanelSize)
+    // this._navigationBlock.classList.add('w-25')
     this._navigationBlock.style.display = 'initial'
     this._buttonExpandNav.style.display = 'initial'
     this._buttonRetractNav.style.display = 'initial'
@@ -76,9 +76,9 @@ class Panels {
   setupRegularNavigator () {
     this._buttonExpandProp.style.display = 'initial'
     if (this._propertiesVisible) { this.setupPropertiesExpand() }
-    this._knotMain.classList.add('w-' + this._knotPanelSize)
-    this._navigationBlock.classList.remove('w-100')
-    this._navigationBlock.classList.add('w-25')
+    // this._knotMain.classList.add('w-' + this._knotPanelSize)
+    // this._navigationBlock.classList.remove('w-100')
+    // this._navigationBlock.classList.add('w-25')
     this._navigationBlock.style.display = 'initial'
     this._buttonExpandNav.style.display = 'initial'
     this._buttonRetractNav.style.display = 'initial'
@@ -89,9 +89,9 @@ class Panels {
       this.setupPropertiesRetract()
       this._propertiesVisible = true
     }
-    this._knotMain.classList.remove('w-' + this._knotPanelSize)
-    this._navigationBlock.classList.remove('w-25')
-    this._navigationBlock.classList.add('w-100')
+    // this._knotMain.classList.remove('w-' + this._knotPanelSize)
+    // this._navigationBlock.classList.remove('w-25')
+    // this._navigationBlock.classList.add('w-100')
     this._buttonExpandNav.style.display = 'none'
     this._buttonRetractNav.style.display = 'initial'
     this._buttonExpandProp.style.display = 'none'
@@ -103,10 +103,12 @@ class Panels {
     this._buttonExpandProp.style.display = 'initial'
     this._buttonCommentsNav.style.display = 'initial'
     this._elementsBlock.style.display = 'none'
-    this._elementsMain.classList.remove('w-25')
-    this._knotMain.classList.remove('w-' + this._knotPanelSize)
-    this._knotPanelSize += 25
-    this._knotMain.classList.add('w-' + this._knotPanelSize)
+    this._elementsMain.style.minWidth = ''
+    this._knotMain.style.width = '100%'
+    // this._elementsMain.classList.remove('w-25')
+    // this._knotMain.classList.remove('w-' + this._knotPanelSize)
+    // this._knotPanelSize += 25
+    // this._knotMain.classList.add('w-' + this._knotPanelSize)
     this._commentsVisible = false
   }
 
@@ -116,10 +118,13 @@ class Panels {
     this._buttonExpandProp.style.display = 'none'
     this._buttonCommentsNav.style.display = 'none'
     this._elementsBlock.style.display = 'initial'
-    this._knotMain.classList.remove('w-' + this._knotPanelSize)
-    this._knotPanelSize -= 25
-    this._knotMain.classList.add('w-' + this._knotPanelSize)
-    this._elementsMain.classList.add('w-25')
+    this._elementsMain.style.minWidth = '25%'
+    this._knotMain.style.width = '75%'
+    // this._knotMain.classList.remove('w-' + this._knotPanelSize)
+    // this._knotPanelSize -= 25
+    // this._knotMain.classList.add('w-' + this._knotPanelSize)
+    // this._elementsMain.classList.add('w-25')
+    // this._elementsMain.style.maxWidth = '25% !important'
   }
 
   setupPropertiesExpand () {
