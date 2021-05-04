9a1bd7edfcf3c17bc6c4f7d3297e33135ecc54f5
Heitor Mattosinho <heitormattosinho@gmail.com>
2021-05-04 06:51:10 -0300

    feat (prognosis.js): expand dependent value children

---
+++ src/adonisjs/public/prognosis/js/prognosis.js
@@ -0,0 +1,111 @@
+class Prognosis {
+  constructor() {
+    window.addEventListener('load', this.start)
+  }
+
+
+  async start (){
+    Prognosis.i.expandMultiChoice()
+  }
+
+  async expandMultiChoice (){
+    var multiValues = document.querySelectorAll('.progn-multi-wrapper')
+    for (var e = 0; e < multiValues.length; e++) {
+      if(multiValues[e].hasAttribute('class') && multiValues[e].classList.contains('progn-multi-wrapper')){
+        var el = document.querySelector('#' + multiValues[e].id.substring(0,(multiValues[e].id.length - 8)))
+        if(el == null){
+          el = document.querySelectorAll('input[name="'+ multiValues[e].id.substring(0,(multiValues[e].id.length - 8)) +'"'+']')
+        }
+
+        const listenerFnMultiChoice = function () {
+          var multiWrapper
+          var _id
+          if(this.getAttribute('type') == 'radio'){
+            _id = this.name
+          }else {
+            _id = this.id
+          }
+          multiWrapper = document.querySelector('#' + _id + '-wrapper')
+          if(this.checked && multiWrapper.classList.contains('d-none')){
+            multiWrapper.classList.remove('d-none')
+          } else if(!this.checked && multiWrapper.classList.contains('d-none') == false){
+            multiWrapper.classList.add('d-none')
+            var inputList = multiWrapper.querySelectorAll('div > input')
+            var childListWrappers = multiWrapper.querySelectorAll(' .progn-multi-wrapper')
+
+            for (var i = 0; i < inputList.length; i++) {
+              inputList[i].checked = false
+            }
+            for (var e = 0; e < childListWrappers.length; e++) {
+              childListWrappers[e].classList.add('d-none')
+            }
+          }
+        }
+
+        function checkMultiChoice (){
+          var multiWrapper
+          var radioSiblingChecked = false
+
+          if(el.length){
+            for (var p = 0; p < el.length; p++) {
+
+              multiWrapper = document.querySelector('#' + el[p].name + '-wrapper')
+              if((el[p].checked && multiWrapper.classList.contains('d-none'))
+              || (radioSiblingChecked && multiWrapper.classList.contains('d-none') == false)){
+                radioSiblingChecked = true
+                multiWrapper.classList.remove('d-none')
+              } else {
+                multiWrapper.classList.add('d-none')
+                var inputList = multiWrapper.querySelectorAll('div > input')
+                var childListWrappers = multiWrapper.querySelectorAll(' .progn-multi-wrapper')
+
+                for (var i = 0; i < inputList.length; i++) {
+                  inputList[i].checked = false
+                }
+                for (var e = 0; e < childListWrappers.length; e++) {
+                  childListWrappers[e].classList.add('d-none')
+                }
+              }
+
+            }
+          }
+          else {
+            multiWrapper = document.querySelector('#' + el.id + '-wrapper')
+
+            if(el.checked && multiWrapper.classList.contains('d-none')){
+              multiWrapper.classList.remove('d-none')
+            } else if(!el.checked && multiWrapper.classList.contains('d-none') == false){
+              multiWrapper.classList.add('d-none')
+              var inputList = multiWrapper.querySelectorAll('div > input')
+              var childListWrappers = multiWrapper.querySelectorAll(' .progn-multi-wrapper')
+
+              for (var i = 0; i < inputList.length; i++) {
+                inputList[i].checked = false
+              }
+              for (var e = 0; e < childListWrappers.length; e++) {
+                childListWrappers[e].classList.add('d-none')
+              }
+            }
+          }
+        }
+        checkMultiChoice()
+
+        if(el.length){
+          for (var i = 0; i < el.length; i++) {
+            el[i].removeEventListener('change', listenerFnMultiChoice)
+            el[i].addEventListener('change', listenerFnMultiChoice)
+          }
+        }else{
+          el.removeEventListener('change', listenerFnMultiChoice)
+          el.addEventListener('change', listenerFnMultiChoice)
+        }
+
+
+      }
+    }
+  }
+
+}
+(function () {
+  Prognosis.i = new Prognosis()
+})()
