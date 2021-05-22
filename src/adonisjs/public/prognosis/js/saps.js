83572c41e6b1ad915c9cf993a826900b3c28b2bc
Heitor Mattosinho <heitormattosinho@gmail.com>
2021-05-22 11:50:10 -0300

    feat (prognosis/player): option type 'select one between'

--- src/adonisjs/public/prognosis/js/saps.js
+++ src/adonisjs/public/prognosis/js/saps.js
@@ -72,9 +72,169 @@ class Saps {
             case 'sida':
               comorb.sida = 8
               break;
-            default:
-
-          }
+            case 'nrc-por-avc':
+              motvAdm.submCirurgiaNrcAvc = 5
+              break;
+            case 'revascularizacao-miocardica':
+              motvAdm.submCirurgiaRevascMiorc = -6
+              break;
+            case 'trauma':
+              motvAdm.submCirurgiaTrauma = -8
+              break;
+            case 'transplante':
+              motvAdm.submCirurgiaTransplante = -11
+              break;
+            case 'outro':
+              motvAdm.submCirurgiaOutro = 0
+              break;
+            case 'arritmia':
+              motvAdm.admClinicaArritmia = -5
+              break;
+            case 'choque-hipovolemico':
+              motvAdm.admClinicaChoqueHipo = 3
+              break;
+            case 'outro-choque':
+              motvAdm.admClinicaOutroChoque = 5
+              break;
+            case 'convulsao':
+              motvAdm.admClinicaColvusao = -4
+              break;
+            case 'abdome-agudo':
+              motvAdm.admClinicaAbdomeAgudo = 3
+              break;
+            case 'pancreatite-grave':
+              motvAdm.admClinicaPancreatite = 9
+              break;
+            case 'deficit-focal':
+              motvAdm.admClinicaDeficitFocal = 7
+              break;
+            case 'efeito-de-massa-intracraniana':
+              motvAdm.admClinicaMassaIntracran = 10
+              break;
+            case 'insuficiencia-hepatica':
+              motvAdm.insuficienciaHepat = 6
+              break;
+            case 'alteracao-do-nivel-de-consciencia':
+              motvAdm.admClinicaAltNvlConsc = 4
+              break;
+            case 'nenhum-dos-anteriores':
+              motvAdm.admClinicaOutro = 0
+              break;
+            case 'Nosocomial':
+                comorb.infectAntesAdm = 4
+              break;
+            case 'Respiratória':
+              comorb.infectAntesAdm = 5
+              break;
+            case '<14 dias':
+                comorb.intAntesAdm = 0
+                break;
+            case '14-27 dias':
+              comorb.intAntesAdm = 6
+              break;
+            case '>=28 dias':
+              comorb.intAntesAdm = 7
+              break;
+            case '3-4':
+              statsClinico.gcs = 15
+              break;
+            case '5':
+              statsClinico.gcs = 10
+              break;
+            case '6':
+              statsClinico.gcs = 7
+              break;
+            case '7-12':
+              statsClinico.gcs = 2
+              break;
+            case '>=13':
+              statsClinico.gcs = 0
+              break;
+            case '<35 °C':
+              statsClinico.temp = 7
+              break;
+            case '>=35 °C':
+              statsClinico.temp = 0
+              break;
+            case '<120 bpm':
+              statsClinico.freqCard = 0
+              break;
+            case '120-159 bpm':
+              statsClinico.freqCard = 5
+              break;
+            case '>=160 bpm':
+              statsClinico.freqCard = 7
+              break;
+            case '<40 mmHg':
+              statsClinico.pressSist = 11
+              break;
+            case '40-69 mmHg':
+              statsClinico.pressSist = 8
+              break;
+            case '70-119 mmHg':
+              statsClinico.pressSist = 3
+              break;
+            case '>=120 mmHg':
+              statsClinico.pressSist = 0
+              break;
+            case '<2 mg/dl':
+              altLab.bilirrubina = 0
+              break;
+            case '2-6 mg/dl':
+              altLab.bilirrubina = 4
+              break;
+            case '>=6 mg/dl':
+              altLab.bilirrubina = 5
+              break;
+            case '<1.2 mg/dl':
+              altLab.creatinina = 0
+              break;
+            case '1.2-1.9 mg/dl':
+              altLab.creatinina = 2
+              break;
+            case '2-3.4 mg/dl':
+              altLab.creatinina = 7
+              break;
+            case '>=3.5 mg/dl':
+              altLab.creatinina = 8
+              break;
+            case '<=7.25':
+              altLab.ph = 3
+              break;
+            case '>7.25':
+              altLab.ph = 0
+              break;
+            case '<15mil /mm³':
+              altLab.leucocitos = 0
+              break;
+            case '>=15mil /mm³':
+              altLab.leucocitos = 2
+              break;
+            case '<20mil /mm³':
+              altLab.plaquetas = 13
+              break;
+            case '20-49mil /mm³':
+              altLab.plaquetas = 8
+              break;
+            case '50-99mil /mm³':
+              altLab.plaquetas = 5
+              break;
+            case '>=100mil /mm³':
+              altLab.plaquetas = 0
+              break;
+            case 'paO2 >=60 sem VM':
+              altLab.oxigenacao = 0
+              break;
+            case 'pa02 <60 sem VM':
+              altLab.oxigenacao = 5
+              break;
+            case 'P/F<100 em VM':
+              altLab.oxigenacao = 11
+              break;
+            case 'P/F >=100 em VM':
+              altLab.oxigenacao = 7
+              break;
+            }
         }
       }
 
