bbe8380e79b0a1a536250c0d4946efa1bba29799
Felipe Arruda <ftarruda@gmail.com>
2021-10-14 19:32:39 -0300

    Adicionando chamada api para o logger a partir do dcc

--- src/adonisjs/public/infra/dcc-logger.js
+++ src/adonisjs/public/infra/dcc-logger.js
@@ -9,10 +9,56 @@ class DCCLogger extends DCCLight {
     this._subscribe('case/summary', this._notifyLogger)
   }
 
+  async _send_message(message) {
+    
+    let request = {
+      "case": 100,
+      "payload": JSON.stringify(message),
+      "origin_ip": "teste_ddc",
+      "event": "chamada ddc"
+    }
+    
+    console.log('request:')
+    console.log(request)
+
+    const config = {
+      method: 'POST',
+      url: 'http://localhost/api/v1/kafka_messages/',
+      withCredentials: false,
+      data: request
+    }
+
+    await axios(config)
+
+    // let response = await fetch('https://localhost/api/v1/kafka_messages/', 
+    //                     {
+    //                       async: true,
+    //                       crossDomain: true,
+    //                       method: 'post',
+    //                       headers: {'Content-Type': 'application/json'},
+    //                       body: JSON.stringify(request) // string or object
+    //                     }
+    //                     ).catch(err => {
+    //                       // trata se alguma das promises falhar
+    //                       console.error('Failed retrieving information', err);
+    //                     });
+    // let js = await response.json();
+    // console.log(js); 
+
+  }
+
   _notifyLogger (topic, message, track) {
     if (track) {
-      console.log('=== logger DCC: ' + topic)
-      console.log(message)
+      
+      try{
+        console.log('=== logger DCC: ' + topic);
+        console.log(message);
+        // this._send_message(message);
+      }catch (e) {
+        console.log('ERROR!');
+        console.log(e);
+      }
+      
     }
   }
 }
