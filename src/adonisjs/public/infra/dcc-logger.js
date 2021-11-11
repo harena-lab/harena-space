39b356cad6e884d7c444ae8ecce01c90e0f72d51
Felipe Arruda <ftarruda@gmail.com>
2021-11-10 21:03:24 -0300

    Modificando dcc-logger para empacotar mensagens

--- src/adonisjs/public/infra/dcc-logger.js
+++ src/adonisjs/public/infra/dcc-logger.js
@@ -6,59 +6,58 @@ class DCCLogger extends DCCLight {
     super()
 
     this._notifyLogger = this._notifyLogger.bind(this)
-    this._subscribe('case/summary', this._notifyLogger)
+    this._subscribe('#', this._notifyLogger)
+
+    this.messages = []
+    this.numberOfMessages = 0
+    this.end_topics = ['knot/end', 'case/summary', 'flow/>/navigate'] // "knot/end" nao existe dessa forma no log, era mais pra representar a ideia do que pode ser um topico que finaliza o empacotamento
   }
 
-  async _send_message(message) {
+  async _send_message() {
     
     let request = {
-      "case": 100,
-      "payload": JSON.stringify(message),
-      "origin_ip": "teste_ddc",
-      "event": "chamada ddc"
-    }
+      "messages": [...this.messages],
+      "version": 0.1
+    };
+    this.numberOfMessages = 0
+    this.messages = []
     
-    console.log('request:')
-    console.log(request)
+    console.log('request:');
+    console.log(request);
 
     const config = {
       method: 'POST',
       url: 'http://localhost/api/v1/kafka_messages/',
       withCredentials: false,
       data: request
-    }
+    };
+    await axios(config);
+  }
 
-    await axios(config)
+  empacota_mensagem (topic, message){
 
-    // let response = await fetch('https://localhost/api/v1/kafka_messages/', 
-    //                     {
-    //                       async: true,
-    //                       crossDomain: true,
-    //                       method: 'post',
-    //                       headers: {'Content-Type': 'application/json'},
-    //                       body: JSON.stringify(request) // string or object
-    //                     }
-    //                     ).catch(err => {
-    //                       // trata se alguma das promises falhar
-    //                       console.error('Failed retrieving information', err);
-    //                     });
-    // let js = await response.json();
-    // console.log(js); 
+    let pacote = {
+      "topic": topic,
+      "payload": message
+    };
 
+    this.messages.push(pacote)
+    this.numberOfMessages += 1;
   }
 
   _notifyLogger (topic, message, track) {
     if (track) {
-      
       try{
         console.log('=== logger DCC: ' + topic);
         console.log(message);
-        // this._send_message(message);
+        this.empacota_mensagem(topic, message);
+        if(this.numberOfMessages > 15 || this.end_topics.includes(topic)){
+          this._send_message();
+        }
       }catch (e) {
         console.log('ERROR!');
         console.log(e);
       }
-      
     }
   }
 }
