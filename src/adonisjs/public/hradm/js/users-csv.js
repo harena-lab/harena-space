a795321c1cf3ee6b346f9794683e347f970c48b1
André Santanchè <santanche@gmail.com>
2022-02-12 10:28:01 -0300

    feat (admin): new user-csv approach

--- src/adonisjs/public/hradm/js/users-csv.js
+++ src/adonisjs/public/hradm/js/users-csv.js
@@ -13,56 +13,64 @@ class UsersCSVManager {
   }
 
   async _addUsers (topic, message) {
-    if (message.value) {
-      const parameters = message.value
-      console.log('===== user parameters')
-      console.log(parameters)
+    let nameC = -1
+    let emailC = -1
+    let schema = this._table[0]
+    for (let s in schema) {
+      switch (schema[s].toLowerCase()) {
+        case 'nome':
+        case 'name': nameC = s; break;
+        case 'e-mail':
+        case 'email': emailC = s; break;
+      }
+    }
+    if (nameC > -1 && emailC > -1) {
       for (let line = 1; line < this._table.length; line++) {
-      if (this._table[line][2]) {
-        let lp = this._table[line][2].substring(0, this._table[line][2].indexOf('@'))
-        console.log('========== creating user ==========')
-        let user = await MessageBus.i.request('user/create/post',
-          {
-            username: this._table[line][1],
-            email: this._table[line][2],
-            password: lp,
-            login: lp,
-            institution: parameters.institution,
-            grade: parameters.grade
-          }
-        )
-        if (user.message.error) {
-          console.log('--- error')
-          console.log(user.message.error)
-        } else {
-          console.log(user.message.username)
-          console.log(user.message.id)
-          let role = await MessageBus.i.request('link/role/post',
+        if (this._table[line][emailC]) {
+          let lp = this._table[line][emailC].substring(0, this._table[line][emailC].indexOf('@'))
+          console.log('========== creating user ==========')
+          let user = await MessageBus.i.request('user/create/post',
             {
-              userId: user.message.id,
-              roleId: parameters.role
+              username: this._table[line][nameC],
+              email: this._table[line][emailC],
+              password: lp,
+              login: this._table[line][emailC],
+              institution: new URL(document.location).searchParams.get('institution'),
+              grade: new URL(document.location).searchParams.get('grade')
             }
           )
-          if (role.message.error) {
+          if (user.message.error) {
             console.log('--- error')
-            console.log(role.message.error)
+            console.log(user.message.error)
           } else {
-            console.log('--- link role success')
-            let group = await MessageBus.i.request('link/group/post',
+            console.log(user.message.username)
+            console.log(user.message.id)
+            let role = await MessageBus.i.request('link/role/post',
               {
                 userId: user.message.id,
-                groupId: parameters.group
+                roleId: new URL(document.location).searchParams.get('role')
               }
             )
-            if (group.message.error) {
+            if (role.message.error) {
               console.log('--- error')
-              console.log(group.message.error)
-            } else
-              console.log('--- link group success')
+              console.log(role.message.error)
+            } else {
+              console.log('--- link role success')
+              let group = await MessageBus.i.request('link/group/post',
+                {
+                  userId: user.message.id,
+                  groupId: new URL(document.location).searchParams.get('group')
+                }
+              )
+              if (group.message.error) {
+                console.log('--- error')
+                console.log(group.message.error)
+              } else
+                console.log('--- link group success')
+            }
           }
         }
       }
-    }
     }
   }
 }
