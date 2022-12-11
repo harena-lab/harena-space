f0e9402ed08fa291c67ca5429ae2ff8d0c7335f2
André Santanchè <santanche@gmail.com>
2022-12-11 12:49:34 -0300

    feat (annotation/memory): new memory panel

---
+++ src/adonisjs/public/editor/annotate/js/memory.js
@@ -0,0 +1,59 @@
+class AnnotatorMemory {
+  async start () {
+    await this._loadMemory()
+    if (this._memory != null)
+      this._updateSummary()
+  }
+
+  async _loadMemory () {
+    const questId = (new URL(document.location)).searchParams.get('questid')
+    let questAnn =
+      await MessageBus.i.request('quest/annotations/get', {quest_id: questId})
+
+    if (questAnn != null && questAnn.message != null) {
+      questAnn = questAnn.message
+      const annotations = []
+      const ifrag = {}
+      for (const c of questAnn) {
+        let slot
+        if (ifrag[c.fragment])
+          slot = ifrag[c.fragment]
+        else {
+          slot = {
+            fragments: [{fragment: c.fragment}],
+            categories: [],
+            count: []}
+          annotations.push(slot)
+          ifrag[c.fragment] = slot
+        }
+        const cat = c.property_id.substring(4)
+        slot.categories.push(cat)
+        slot.count.push(c.count)
+      }
+      this._memory = annotations
+    }
+  }
+
+  _updateSummary () {
+    const annotations = this._memory
+    let html = '<table>'
+    for (const an of annotations) {
+      html += '<tr><td><table>'
+      for (const f of an.fragments)
+        html += '<tr><td>' + f.fragment + '</td></tr>'
+      html += '</table></td><td><table>'
+      let index = 0
+      for (const c of an.categories) {
+        html += '<tr><td>' + c + '</td><td>' + an.count[index] + '</td></tr>'
+        index++
+      }
+      html += '</td></tr></table>'
+    }
+    html += '</table>'
+    document.querySelector('#memory-details').innerHTML = html
+  }
+}
+
+(function () {
+  AnnotatorMemory.i = new AnnotatorMemory()
+})()
