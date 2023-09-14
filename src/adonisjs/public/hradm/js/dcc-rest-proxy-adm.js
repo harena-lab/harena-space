82d0b69d4b7e808db43284faafc145bb4d18527a
Heitor Mattosinho <heitormattosinho@gmail.com>
2023-09-14 14:05:37 -0300

    feat (hradm/tokenLogin): resolving conflict before merge

--- src/adonisjs/public/hradm/js/dcc-rest-proxy-adm.js
+++ src/adonisjs/public/hradm/js/dcc-rest-proxy-adm.js
@@ -139,29 +139,6 @@ DCC.component(
 )
 
 DCC.component(
-  'harena-hradm-groups',
-  'dcc-rest',
-  {
-    environment: {
-      'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api,
-    },
-    oas: {
-      paths: {
-        '{url-manager}/admin/groups': {
-          'get': {
-            operationId: 'groups-list',
-            parameters: [
-              {name: 'url-manager',
-               in: 'path'}
-            ]
-          }
-        }
-      }
-    }
-  }
-)
-
-DCC.component(
   'harena-hradm-roles',
   'dcc-rest',
   {
@@ -176,6 +153,122 @@ DCC.component(
             parameters: [
               {name: 'url-manager',
                in: 'path'}
+            ]
+          }
+        }
+      }
+    }
+  }
+)
+
+//Retrieves all groups from database
+DCC.component(
+  'harena-admin-groups',
+  'dcc-rest',
+  {
+    environment: {
+      'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api,
+    },
+    oas: {
+      paths: {
+        '{url-manager}/admin/groups': {
+          'get': {
+            operationId: 'admin-group-list',
+            parameters: [
+              {name: 'url-manager',
+               in: 'path'}
+            ]
+          }
+        }
+      }
+    }
+  }
+)
+
+DCC.component(
+  'harena-admin-users',
+  'dcc-rest',
+  {
+    environment: {
+      'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api,
+      'fInstitution': new URL(document.location).searchParams.get('fInstitution'),
+      'fUserType': new URL(document.location).searchParams.get('fUserType'),
+      'fGroup': new URL(document.location).searchParams.get('fGroup'),
+      'fSearchStr': new URL(document.location).searchParams.get('fSearchStr'),
+      'page': new URL(document.location).searchParams.get('page'),
+      'nItems': new URL(document.location).searchParams.get('limit'),
+    },
+    oas: {
+      paths: {
+        '{url-manager}/admin/users': {
+          'get': {
+            operationId: 'admin-users-list',
+            parameters: [
+              {name: 'url-manager',
+                in: 'path'},
+              {name: 'fInstitution',
+                in: 'query'},
+              {name: 'fUserType',
+                in: 'query'},
+              {name: 'fGroup',
+                in: 'query'},
+              {name: 'fSearchStr',
+                in: 'query'},
+              {name: 'page',
+                in: 'query'},
+              {name: 'nItems',
+                in: 'query'},
+            ]
+          }
+        }
+      }
+    }
+  }
+)
+
+DCC.component(
+  'harena-admin-gen-login-token',
+  'dcc-rest',
+  {
+    environment: {
+      'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api,
+    },
+    oas: {
+      paths: {
+        '{url-manager}/admin/user/temp_login': {
+          'put': {
+            operationId: 'admin-gen-login-token',
+            parameters: [
+              {name: 'url-manager',
+                in: 'path'},
+              {name: 'userId',
+                in: 'query'},
+
+            ]
+          }
+        }
+      }
+    }
+  }
+),
+
+DCC.component(
+  'harena-admin-user-info',
+  'dcc-rest',
+  {
+    environment: {
+      'url-manager': HarenaConfig.manager.url + HarenaConfig.manager.api,
+    },
+    oas: {
+      paths: {
+        '{url-manager}/user/{userId}': {
+          'get': {
+            operationId: 'admin-user-info',
+            parameters: [
+              {name: 'url-manager',
+                in: 'path'},
+              {name: 'userId',
+                in: 'path'},
             ]
           }
         }
