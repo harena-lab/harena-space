e660df210538d9f4df4bda467729463c400b84aa
André Santanchè <santanche@gmail.com>
2025-03-19 09:11:24 -0300

    feat (track): new track

--- src/adonisjs/public/player/js/htracker.js
+++ src/adonisjs/public/player/js/htracker.js
@@ -21,7 +21,8 @@ class Tracker {
     MessageBus.i.subscribe('input/summary', this.inputSummary)
 
     this.knotStart = this.knotStart.bind(this)
-    MessageBus.i.subscribe('knot/start/#', this.knotStart)
+    MessageBus.i.subscribe('knot/navigate/#', this.knotStart)
+    MessageBus.i.subscribe('flow/navigate/+', this.knotStart)
     this.caseCompleted = this.caseCompleted.bind(this)
     this.sessionRound = this.sessionRound.bind(this)
     MessageBus.i.subscribe('case/completed/+', this.caseCompleted)
@@ -186,6 +187,14 @@ class Tracker {
     this._knotTrack.push(kt)
     this._trackStore()
     this._publishDetails({knotTrack: kt}, this._userId, this._caseId)
+    if (message != null) {
+      MessageBus.i.publish('case/summary/' + MessageBus.extractLevel(topic, 3),
+          {userId: message.userId,
+          caseId: message.caseId,
+          knotTrack: this._knotTrack,
+          variables: this._variables,
+          varTrack: this._varTrack}, true)
+    }
   }
 
   inputSummary (topic, message) {
